1bfaa876a746b5a4993b1f5fcf2f80a6400e2b4a
Nakuru Wubni <nwubni@gmail.com>
2023-04-25 13:39:49 +0100

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -69,10 +69,6 @@ int _printf(const char *format, ...)
 				case '%':
 				count += _putchar('%');
 				break;
-				default:
-				count += _putchar('%');
-				count += _putchar(*format);
-				break;
 			}
 		}
 		else
